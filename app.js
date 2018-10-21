0215c7b45c07480a5b0b9a87353be1b41631a39f
JackMacWindows <jackbruienne@yahoo.com>
2018-10-21 17:46:33 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+http.createServer(function(req, res) {
+    if (req.url == '/favicon.ico') return
+    var location = "https://www.reddit.com/r" + req.url
+    if (req.url == '/') location = "https://www.reddit.com/"
+    const head = {
+        'Location': location
+    }
+    res.writeHead(302, head)
+}).listen(8081);
